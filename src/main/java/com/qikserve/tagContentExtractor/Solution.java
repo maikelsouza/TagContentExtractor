a3a4b131c95fbc7101777ec1c2ee27542921e3da
RE045908 <re045908@qintess.com>
2024-12-10 08:05:27 -0300

    first commit

---
+++ src/main/java/com/qikserve/tagContentExtractor/Solution.java
@@ -0,0 +1,74 @@
+package com.qikserve.tagContentExtractor;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public static void main( String[] args ) {
+
+        Scanner in = new Scanner(System.in);
+        int numberOfSentences = Integer.parseInt(in.nextLine());
+
+        if(numberOfSentences > 0){
+            while (numberOfSentences > 0) {
+                String line = in.nextLine();
+
+                List<String> openingTags = findOpeningTags(line);
+                List<String> closingTags = findClosingTags(line);
+
+                if (openingTags.size() != closingTags.size()){
+                    System.out.println("None");
+                }else{
+                    for (int i = 0; i< openingTags.size(); i++){
+                        String openingTag = openingTags.remove(i);
+                        String closingTag = closingTags.remove(i);
+
+                        if (openingTag.equals(closingTag.replace("/",""))){
+                            String extractTextBetweenTags = extractTextBetweenTags(line,openingTag,closingTag);
+                            System.out.println(extractTextBetweenTags);
+                        }else {
+                            System.out.println("None");
+                        }
+                    }
+                }
+                numberOfSentences--;
+            }
+            in.close();
+        }
+    }
+
+    private static List<String> findOpeningTags(String args) {
+        return findTags(args, "<([a-zA-Z0-9]+)[^>]*>");
+    }
+
+    private static List<String> findClosingTags(String args) {
+        return findTags(args, "</([a-zA-Z0-9]+)[^>]*>");
+    }
+
+    private static List<String> findTags(String args, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(args);
+
+        List<String> tags = new ArrayList<>();
+        while (matcher.find()) {
+            tags.add(matcher.group());
+        }
+
+        return tags;
+    }
+
+    private static String extractTextBetweenTags(String input, String openingTag, String closingTag) {
+
+        String regex = Pattern.quote(openingTag) + "(.*?)" + Pattern.quote(closingTag);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            return matcher.group(1).replaceAll("<.*?>", "");
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/test/java/com/qikserve/tagContentExtractor/SolutionTest.java
@@ -0,0 +1,70 @@
+package com.qikserve.tagContentExtractor;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+
+    @Test
+    public void testExampleFromHackerRank() {
+        String input = "4\n" +
+                "<h1>Nayeem loves counseling</h1>\n" +
+                "<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>\n" +
+                "<Amee>safat codes like a ninja</amee>\n" +
+                "<SA premium>Imtiaz has a secret crush</SA premium>\n";
+
+        String expectedOutput = "Nayeem loves counseling\n" +
+                "Sanjay has no watch\n" +
+                "So wait for a while\n" +
+                "None\n" +
+                "Imtiaz has a secret crush";
+
+        String actualOutput = runTest(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testValidTags() {
+        String input = "2\n" +
+                "<h1>Nayeem loves counseling</h1>\n" +
+                "<SA premium>Imtiaz has a secret crush</SA premium>\n";
+
+        String expectedOutput = "Nayeem loves counseling\n" +
+                "Imtiaz has a secret crush";
+
+        String actualOutput = runTest(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testInvalidTags() {
+        String input = "1\n" +
+                "<h1>Nayeem loves counseling</h2>\n";
+
+
+        String expectedOutput = "None";
+
+        String actualOutput = runTest(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    private String runTest(String input) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        Solution.main(null);
+
+        return outputStream.toString().trim().replace("\r\n", "\n");
+    }
+
+
+}
